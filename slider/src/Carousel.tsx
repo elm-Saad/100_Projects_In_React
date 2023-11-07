7e050db92a0aa0f60c5d281748bf93e6b9351bef
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2023-11-07 04:16:07 +0100

    finish the carousel project

--- slider/src/Carousel.tsx
+++ slider/src/Carousel.tsx
@@ -1,18 +1,44 @@
-import {useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import { longList, shortList, list } from "./data"
 import { DataType } from './interfaces'
 import {FaQuoteRight} from 'react-icons/fa'
 import {FiChevronLeft,FiChevronRight} from 'react-icons/fi'
 const Carousel = () => {
-  const [ListData,setListData] = useState<DataType[]>(list)
+  const [ListData,setListData] = useState<DataType[]>(longList)
+  const [currentItem,setCurrentItem] = useState<number>(2)
 
+  const nextSide = () =>{
+    setCurrentItem( (prev) =>{
+      return (prev + 1) % (ListData.length)
+    })
+
+  }
+  const prevSlide = () => {
+    setCurrentItem( (prev) =>{
+      return (prev - 1 + ListData.length) % (ListData.length)
+    })
+  }
+
+  useEffect(()=>{
+    let intervalId = setInterval(()=>{
+      nextSide()
+    },2500)
+
+    return ()=>{
+      clearInterval(intervalId)
+    }
+  },[currentItem])
   return(
     <div className='select-none w-full h-96 bg-white flex items-center justify-center '>
-      <section className='w-full h-96 max-w-[1000px] min-w-[300px] relative'>
-        {ListData.map(item =>{
+      <section className='w-full h-96 max-w-[1000px] min-w-[300px] relative overflow-hidden'>
+        {ListData.map((item,index) =>{
           return <div 
             key={item.id}
-            className='absolute top-0 left-0 border bg-white shadow-md rounded-lg m-8 p-8'
+            className='absolute top-0 left-0 border bg-white shadow-md rounded-lg m-8 p-8 transition duration-1000'
+            style={{
+              transform:`translateX(${100*(index - currentItem)}%)`,
+              opacity:(index === currentItem)?'1':'0'
+            }}
           >
             <div className='relative flex items-center flex-col'>
                 <img src={item.image} alt={item.name} className=' rounded-full w-32 h-32' />
@@ -20,10 +46,17 @@ const Carousel = () => {
                 <p className='text-gray-600 font-semibold'>{item.title}</p>
                 <p className='select-text text-gray-500 font-semibold'>{item.quote}</p>
                 <FaQuoteRight className='text-blue-600 text-2xl'/>
-                <button className='p-1 absolute left-4 top-1/3 rounded-md transition hover:bg-blue-400 hover:text-white'>
+                <button 
+                  className='p-1 absolute left-4 top-1/3 rounded-md transition hover:bg-blue-400 hover:text-white'
+                  onClick={()=>prevSlide()}
+                >
                 <FiChevronLeft className='w-6 h-6' />
                 </button>
-                <button className='p-1 absolute right-4 top-1/3 rounded-md transition hover:bg-blue-400 hover:text-white'>
+                <button 
+                  className='p-1 absolute right-4 top-1/3 rounded-md transition hover:bg-blue-400 hover:text-white'
+                  onClick={()=>nextSide()}
+
+                >
                 <FiChevronRight className='w-6 h-6'/>
                 </button>
             </div>
