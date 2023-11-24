64f7ec15add25a3ea67427ba302b377c10c3bb0f
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2023-11-24 16:32:31 +0100

    Initial Files For CMS Project

---
+++ CMS_Project/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App =()=>{
+  return <main className="min-h-screen w-full flex justify-center">
+    <section className="w-full max-w-[2200px] flex flex-col items-center">
+     hi CMS project
+    </section>
+  </main>
+}
+
+export default App
