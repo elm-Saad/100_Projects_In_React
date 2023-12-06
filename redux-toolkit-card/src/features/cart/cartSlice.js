f7dad0bd37d359fb3a6f1a607bcc28e9e6dba913
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2023-12-06 08:27:39 +0100

    remove item

--- redux-toolkit-card/src/features/cart/cartSlice.js
+++ redux-toolkit-card/src/features/cart/cartSlice.js
@@ -15,16 +15,27 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState,
 
-    // pass the reducer in redux
+    // pass the reducer in redux is like 
+    // pass deferent functionality and have access to the state and the payload then export ;to use it 
+    // calling the hook useDispatch and run it 
     reducers: {
-        clearCart: (state) => {
-          state.cartItems = []
-        },
+      clearCart: (state) => {
+        state.cartItems = []
+
+        // what ever get return from the reducer is going to be the new state
+
+      },
+      removeItem:(state,action) => {
+        const id = action.payload
+        // to use the modified state every time it change  get access to state.___
+        state.cartItems = state.cartItems.filter(item=>item.id != id)
+      }
+
     },
 })
 
 
 // action set by default by immer library behind 
-export const { clearCart } = cartSlice.actions
+export const { clearCart,removeItem } = cartSlice.actions
 
 export default cartSlice.reducer
