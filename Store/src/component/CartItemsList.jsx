d0f10c838517d68d5e2b06b444b6747f75f2ef13
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2023-12-11 12:37:33 +0100

    add cart item

--- Store/src/component/CartItemsList.jsx
+++ Store/src/component/CartItemsList.jsx
@@ -1,7 +1,16 @@
+import { useSelector } from 'react-redux'
+import CartItem from './CartItem'
 
 
-const CartItemsList = () =>{
-    return <p>CartItemsList</p>
+const CartItemsList = () => {
+  const cartItems = useSelector((state) => state.cartState.cartItems);
+
+  return (
+    <div>
+      {cartItems.map((item) => {
+        return <CartItem key={item.cartID} cartItem={item} />
+      })}
+    </div>
+  )
 }
-
 export default CartItemsList
