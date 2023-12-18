a7508d5ff22596386a0ada97fbb68c5a5ea1734a
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2023-12-18 07:53:00 +0100

    update

--- jobster/src/pages/landing.jsx
+++ jobster/src/pages/landing.jsx
@@ -3,7 +3,23 @@ import main from '../assets/images/main.svg'
 
 
 const Landing = () => {
-    return <p>Landing page</p>
+    return <main>
+        <nav>
+          <img src={logo} alt='jobster logo' className='logo' />
+        </nav>
+        <div className='container page'>
+          {/* info */}
+          <div className='info'>
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>some text</p>
+            <button className='btn btn-hero'>Login/Register</button>
+          </div>
+          <img src={main} alt='job hunt' className='img main-img' />
+        </div>
+      </main>
+    
 }
-
+  
 export default Landing
