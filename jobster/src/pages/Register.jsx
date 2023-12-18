67dd864643c54f2e33c3029d91a6a55787b91f5e
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2023-12-18 13:25:24 +0100

    update

--- jobster/src/pages/Register.jsx
+++ jobster/src/pages/Register.jsx
@@ -1,6 +1,38 @@
+import { useState, useEffect } from 'react'
+import { FormRow, Logo } from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
 
-
+const initialState = {
+    name:'',
+    email:'',
+    password:'',
+    isMember:true
+}
 const Register = () =>{
-    return <p>Register</p>
+    const [values,setValues] = useState(initialState)
+
+    const handleChange = (e) =>{
+        setValues(e.target.value)
+    }
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
+
+
+    return <Wrapper className='full-page'>
+    <form className='form' onSubmit={handleSubmit}>
+      <Logo />
+      <h3>Login</h3>
+
+      {/* fields */}
+      <FormRow type={'text'} name={'name'} value={values.name} handleChange={handleChange} />
+      <FormRow type={'text'} name={'name'} value={values.name} handleChange={handleChange} />
+      <FormRow type={'text'} name={'name'} value={values.name} handleChange={handleChange} />
+
+      <button type='submit' className='btn btn-block'>
+        submit
+      </button>
+    </form>
+  </Wrapper>
 }
 export default Register
