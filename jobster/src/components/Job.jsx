da007700dae839c7d96f11af4d98d28c1537a836
elm-Saad <128216930+elm-Saad@users.noreply.github.com>
2023-12-22 11:30:24 +0100

    update

--- jobster/src/components/Job.jsx
+++ jobster/src/components/Job.jsx
@@ -4,8 +4,49 @@ import Wrapper from '../assets/wrappers/Job'
 import { useDispatch } from 'react-redux'
 
 
-const Job = ()=>{
-    return <p>job</p>
+const Job = ({_id,position,company,jobLocation,jobType,createdAt,status,})=>{
+
+    const dispatch = useDispatch()
+
+    return (
+        <Wrapper>
+          <header>
+            <div className='main-icon'>{company.charAt(0)}</div>
+            <div className='info'>
+              <h5>{position}</h5>
+              <p>{company}</p>
+            </div>
+          </header>
+          <div className='content'>
+            <div className='content-center'>
+              <h4>more content</h4>
+              <div className={`status ${status}`}>{status}</div>
+            </div>
+            <footer>
+              <div className='actions'>
+                <Link
+                  to='/add-job'
+                  className='btn edit-btn'
+                  onClick={() => {
+                    console.log('edit job');
+                  }}
+                >
+                  Edit
+                </Link>
+                <button
+                  type='button'
+                  className='btn delete-btn'
+                  onClick={() => {
+                    console.log('delete  job');
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </footer>
+          </div>
+        </Wrapper>
+    )
 }
 
 export default Job
